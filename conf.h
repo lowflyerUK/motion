d7371bd81a5c6cbd7d4dd1e84348232c058d1a06
dozencrows <git@magwitch.uk.net>
2013-06-29 14:36:43 +0100

    added simple camera plugin model refactored mmal camera into plugin added raw input capture to mmal camera plugin added filecam for playing back raw input capture added simple metrics module added ARM assembler implementations of simple image diff and reference image update added config files to drive capture and playback for testing and profiling work-in-progress on using still camera capture (not yet working)

--- conf.h
+++ conf.h
@@ -128,10 +128,12 @@ struct config {
     const char *netcam_proxy;
     unsigned int netcam_tolerant_check;
     unsigned int rtsp_uses_tcp;
+    const char *filecam_path;
 #ifdef HAVE_MMAL
     const char *mmalcam_name;
     const char *mmalcam_control_params;
     int mmalcam_use_still;
+    const char *mmalcam_raw_capture_file;
 #endif
     int text_changes;
     const char *text_left;
