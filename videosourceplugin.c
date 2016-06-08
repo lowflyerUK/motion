408fda50c451915eca2d53b4e074057ea3f3af07
Oliver Dewdney <ojd-github@dewdney.co.uk>
2016-06-08 07:57:09 +0000

    made mmal priority

--- videosourceplugin.c
+++ videosourceplugin.c
@@ -13,15 +13,15 @@ void video_source_plugins_init(struct context *cnt) {
 
     struct config *conf = &cnt->conf;
 
-    if (conf->filecam_path) {
-        filecam_select_as_plugin(cnt);
-        return;
-    }
 #ifdef HAVE_MMAL
     if (conf->mmalcam_name) {
         mmalcam_select_as_plugin(cnt);
         return;
     }
 #endif
+    if (conf->filecam_path) {
+        filecam_select_as_plugin(cnt);
+        return;
+    }
 }
 
